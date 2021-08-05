7e490f54355193dfbef1a9aac93cf20c377abde9
Esteban <me@estebansanti.com>
2021-08-04 23:46:02 -0400

    bug fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const cache = require('./cache')
 module.exports = (key, value, exp, callback) => {
-    if (key && value) {
+    if (key && typeof value != 'undefined') {
         cache.put(key, value, exp, function(key, value) {
             if (callback) callback(key, value)
         })
         return
     }
-    if (key && !value) {
+    if (key && typeof value == 'undefined') {
         return cache.get(key)
     }
 }
